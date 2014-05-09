4d584f419e92b4ee688dca4c01b74cfc1217f828
nar <s1326411@gmail.com>
2014-05-09 18:06:08 +0900

    Modify ndenv installation in mac

--- setup.sh
+++ setup.sh
@@ -19,16 +19,16 @@ case ${OSTYPE} in
     brew update
     brew install rbenv ruby-build
     brew install pyenv
-    brew install ndenv node-build
     ;;
   linux*)
     git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
     git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-    git clone https://github.com/riywo/ndenv ~/.ndenv
-    git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
     ;;
 esac
+
+git clone https://github.com/riywo/ndenv ~/.ndenv
+git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
 
 # neobundle
 # curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
