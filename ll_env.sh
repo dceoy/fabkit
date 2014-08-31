17a365a3ee08b399d2a7def405dd9fd995a0fb3b
r <r@localhost.localdomain>
2014-09-01 01:09:48 +0900

    Add ll_env.sh

---
+++ ll_env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# python
+pip install tornado flake8 awscli pyzmq jinja2 ipython numpy scipy matplotlib scikit-learn pandas pyper
+
+# ruby
+gem install bundler rails compass middleman
+
+# node
+npm install -g yo
