d339ce320bc979e6cdd0b2e07623b5f06259dd06
dceoy <d.narsil@gmail.com>
2014-12-25 17:32:46 +0900

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,18 +4,73 @@
 # fabfile.py
 #
 
-from fabric.api import sudo
+import yaml
+from fabric.api import sudo, run
+# from fabric.contrib.console import confirm
+
+
+with open('config.yml') as f:
+  cfg = yaml.load(f)
+
+
+def yum():
+  sudo("yum -y update")
+  sudo("yum -y groupinstall '%s'" % '\' \''.join(cfg['yum_group']))
+  sudo("yum -y install %s" % ' '.join(cfg['yum']))
+
+
+def set_user(user):
+  sudo("usermod -G wheel %s" % user)
+  sudo("chsh -s `which zsh` %s" % user)
 
 
 def dev_env():
-  yum_grps = ('\'Development Tools\'', '\'C Development Tools and Libraries\'')
-  yum_pkgs = ('install', 'zsh', 'vim', 'wget', 'curl', 'tree', 'nkf', 'wol', 'tmux', 'postgresql', 'postgresql-devel', 'sqlite-devel', 'openssl-devel', 'readline-devel', 'bzip2-devel', 'libxml2-devel', 'libxslt-devel', 'libsqlite3x-devel', 'gcc-gfortran', 'atlas-sse3-devel', 'libpng-devel', 'freetype-devel', 'R', 'python3-devel', 'redis', 'mongodb', 'openmpi', 'openmpi-devel')
+  run("git config --global color.ui true")
+  run("git clone https://github.com/dceoy/dotfiles.git ~/dotfiles")
 
-  sudo("yum -y update")
-  sudo("yum -y groupinstall %s" % ' '.join(yum_grps))
-  sudo("yum -y install %s" % ' '.join(yum_pkgs))
+  dots = ('.zshrc', '.zshenv', '.vimrc', '.gemrc')
+  for d in dots:
+    run("ln -sf ~/dotfiles/d%s ~/%s" % (d, d))
 
-def chsh_zsh(user):
-  sudo("yum -y install zsh")
-  sudo("chsh -s `which zsh` %s" % (zsh_path, user))
+  run("git clone https://github.com/sstephenson/rbenv.git ~/.rbenv")
+  run("git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build")
+  run("git clone https://github.com/yyuu/pyenv.git ~/.pyenv")
+  run("esac")
+  run("git clone https://github.com/riywo/ndenv ~/.ndenv")
+  run("git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build")
+  run("mkdir -p ~/.vim/bundle")
+  run("git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim")
 
+
+def osx_env():
+  # run("ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'")
+  run("brew update")
+  for f in cfg['brew']:
+    run("brew install %s" % f)
+
+  run("git clone https://github.com/riywo/ndenv ~/.ndenv")
+  run("git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build")
+  run("curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh")
+
+
+def pip():
+  for p in cfg['pip']:
+    run("pip install %s" % p)
+
+
+def gem():
+  for g in cfg['gem']:
+    run("gem install --no-ri --no-rdoc %s" % g)
+
+
+def npm():
+  for n in cfg['npm']:
+    run("npm install -g %s" % n)
+
+
+def git_user(user):
+  run("git config --global user.name %s" % user)
+
+
+def git_email(email):
+  run("git config --global user.email %s" % email)
--- ll_env.sh
+++ ll_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # python
-pip install tornado flake8 awscli pyzmq jinja2 ipython numpy scipy matplotlib scikit-learn pandas pyper
+pip install tornado flake8 awscli pyzmq jinja2 ipython numpy scipy matplotlib scikit-learn pandas pyper pyyaml fabric
 
 # ruby
 gem install bundler rails compass middleman
