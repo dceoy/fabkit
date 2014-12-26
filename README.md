c09b08fcc129dc6efb9e71fdc72c76a93fa557ab
dceoy <d.narsil@gmail.com>
2014-12-26 15:44:12 +0900

    Integrate scripts into fabfile.py

--- README.md
+++ README.md
@@ -6,25 +6,14 @@ Profiles of Zsh, Vim, etc.
 Construction of Development Environment
 ---------------------------------------
 
-Instralling Zsh, Vim, Other Tools (for RHEL)
+In RHEL (local or remote)
 
 ```sh
-su
-bash rhel_su.sh
+fab -u [user name] -H [host address] init_rhel_env
 ```
 
-Setup of Zsh and Vim
+In MacOSX (local)
 
 ```sh
-cd
-git clone https://github.com/d4i/dotfiles.git
-bash dev_env.sh
+fab -u [user name] -H localhost init_osx_env
 ```
-
-Attention: To allow the new links, setup.sh forces the following files to be removed if they exist.
-
-* ~/.zshrc
-* ~/.zshenv
-* ~/.zprofile
-* ~/.vimrc
-* ~/.gemrc
--- fabfile.py
+++ fabfile.py
@@ -4,73 +4,110 @@
 # fabfile.py
 #
 
+
 import yaml
-from fabric.api import sudo, run
-# from fabric.contrib.console import confirm
+from fabric.api import sudo, run, settings, task
 
 
+# env variables
+
+env_user = run("echo $USER")
 with open('config.yml') as f:
-  cfg = yaml.load(f)
+  env_config = yaml.load(f)
 
 
-def yum():
-  sudo("yum -y update")
-  sudo("yum -y groupinstall '%s'" % '\' \''.join(cfg['yum_group']))
-  sudo("yum -y install %s" % ' '.join(cfg['yum']))
+# general
+
+@task
+def git_user(user):
+  run("git config --global user.name %s" % user)
 
 
-def set_user(user):
-  sudo("usermod -G wheel %s" % user)
-  sudo("chsh -s `which zsh` %s" % user)
+@task
+def git_email(email):
+  run("git config --global user.email %s" % email)
 
 
-def dev_env():
+def dotf():
   run("git config --global color.ui true")
   run("git clone https://github.com/dceoy/dotfiles.git ~/dotfiles")
 
-  dots = ('.zshrc', '.zshenv', '.vimrc', '.gemrc')
-  for d in dots:
-    run("ln -sf ~/dotfiles/d%s ~/%s" % (d, d))
+  for f in ('.zshrc', '.zshenv', '.vimrc', '.gemrc'):
+    run("ln -s ~/dotfiles/%s ~/%s" % ('d' + f, f))
+
+
+def lang_env():
+  run("source ~/.zshenv")
+
+  py2 = env_config['ver']['python2']
+  py3 = env_config['ver']['python3']
+  run("pyenv install %s && pyenv rehash" % py2)
+  run("pyenv install %s && pyenv rehash" % py3)
+  run("pyenv global %s" % py3)
+
+  rb = env_config['ver']['ruby']
+  run("rbenv install %s && rbenv rehash" % rb)
+  run("rbenv global %s" % rb)
+
+  nd = env_config['ver']['nodejs']
+  run("ndenv install %s && ndenv rehash" % nd)
+  run("ndenv global %s" % nd)
+
+  with settings(warn_only=True):
+    for p in env_config['pip']:
+      run("pip install %s" % p)
+    for g in env_config['gem']:
+      run("gem install --no-ri --no-rdoc %s" % g)
+    for n in env_config['npm']:
+      run("npm install -g %s" % n)
+
+
+# rhel
+
+@task
+def init_rhel_env():
+  sudo("yum -y update")
+  sudo("yum -y groupinstall '%s'" % '\' \''.join(env_config['yum_group']))
+  sudo("yum -y install %s" % ' '.join(env_config['yum']))
+
+  sudo("usermod -G wheel %s" % env_user)
+  sudo("chsh -s `which zsh` %s" % env_user)
+
+  dotf()
 
   run("git clone https://github.com/sstephenson/rbenv.git ~/.rbenv")
   run("git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build")
   run("git clone https://github.com/yyuu/pyenv.git ~/.pyenv")
-  run("esac")
   run("git clone https://github.com/riywo/ndenv ~/.ndenv")
   run("git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build")
   run("mkdir -p ~/.vim/bundle")
   run("git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim")
 
+  lang_env()
 
-def osx_env():
-  # run("ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'")
-  run("brew update")
-  for f in cfg['brew']:
+  run("vim -c NeoBundleInstall -c q")
+
+
+# osx
+
+@task
+def init_osx_env():
+  with settings(warn_only=True):
+    r = run("brew --version")
+  if r.failed:
+    run("ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'")
+  else:
+    run("brew update")
+
+  for f in env_config['brew']:
     run("brew install %s" % f)
+
+  dotf()
 
   run("git clone https://github.com/riywo/ndenv ~/.ndenv")
   run("git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build")
   run("curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh")
 
+  lang_env()
 
-def pip():
-  for p in cfg['pip']:
-    run("pip install %s" % p)
-
-
-def gem():
-  for g in cfg['gem']:
-    run("gem install --no-ri --no-rdoc %s" % g)
-
-
-def npm():
-  for n in cfg['npm']:
-    run("npm install -g %s" % n)
-
-
-def git_user(user):
-  run("git config --global user.name %s" % user)
-
-
-def git_email(email):
-  run("git config --global user.email %s" % email)
+  run("vim -c NeoBundleInstall -c q")
