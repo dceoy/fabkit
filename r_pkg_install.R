690db2dad1d2a93f15b16c41e619973a182ded22
dceoy <d.narsil@gmail.com>
2015-11-17 03:26:33 +0900

    Improve R package installation

--- r_pkg_install.R
+++ r_pkg_install.R
@@ -3,61 +3,68 @@
 # cran
 options(repos = 'http://cran.rstudio.com/')
 if(length(list.files(.libPaths()[1])) != 0) update.packages(checkBuilt = TRUE, ask = FALSE)
+load_cr <- function(pkgs) {
+  sapply(as.vector(pkgs),
+         function(pkg) {
+           if(! pkg %in% installed.packages()[, 1]) install.packages(pkg, dep = TRUE)
+           require(pkg, character.only = TRUE)
+         })
+}
 
 # devtools
-if(! 'devtools' %in% installed.packages()[, 1]) install.packages('devtools', dep = TRUE)
+load_cr('devtools')
+load_gh <- function(pkgs) {
+  devtools::install_github(pkgs)
+  sapply(as.vector(names(pkgs)),
+         function(pkg) {
+           require(pkg, character.only = TRUE)
+         })
+}
 
 # bioconductor
 source('http://bioconductor.org/biocLite.R')
 biocLite()
-
-# packages
-df_pkg <- rbind(data.frame(name = c('dplyr',
-                                    'tidyr',
-                                    'data.table',
-                                    'yaml',
-                                    'snow',
-                                    'foreach',
-                                    'doSNOW',
-                                    'ggplot2',
-                                    'ggmcmc',
-                                    'ggdendro',
-                                    'gridExtra',
-                                    'glmmML',
-                                    'MCMCpack',
-                                    'rstan',
-                                    'coin',
-                                    'ranger',
-                                    'abc',
-                                    'phangorn',
-                                    'amap',
-                                    'GMD',
-                                    'RSQLite',
-                                    'plyr',
-                                    'shiny',
-                                    'rvest',
-                                    'rbenchmark',
-                                    'rmarkdown'),
-                           repos = 'cran'),
-                data.frame(name = c('klutometis/roxygen'),
-                           repos = 'github'),
-                data.frame(name = c('PhViD',
-                                    'Biobase',
-                                    'GEOquery'),
-                           repos = 'bioc'))
+load_bc <- function(pkgs) {
+  sapply(as.vector(pkgs),
+         function(pkg) {
+           if(! pkg %in% installed.packages()[, 1]) biocLite(pkg)
+           require(pkg, character.only = TRUE)
+         })
+}
 
 # install
-apply(df_pkg,
-      1,
-      function(pkg) {
-        if(! pkg['name'] %in% installed.packages()[, 1]) {
-          switch(pkg['repos'],
-                 'cran' = install.packages(pkg['name'], dep = TRUE),
-                 'github' = devtools::install_github(pkg['name']),
-                 'bioc' = biocLite(pkg['name']))
-        }
-      })
-
-# load
-print(sapply(as.vector(df_pkg$name),
-             function(pkg) require(pkg, character.only = TRUE)))
+pkgs <- list(cr = c('dplyr',
+                    'tidyr',
+                    'data.table',
+                    'yaml',
+                    'snow',
+                    'foreach',
+                    'doSNOW',
+                    'ggplot2',
+                    'ggmcmc',
+                    'ggdendro',
+                    'gridExtra',
+                    'glmmML',
+                    'MCMCpack',
+                    'rstan',
+                    'coin',
+                    'ranger',
+                    'abc',
+                    'phangorn',
+                    'amap',
+                    'GMD',
+                    'RSQLite',
+                    'h2o',
+                    'plyr',
+                    'shiny',
+                    'rvest',
+                    'rbenchmark',
+                    'rmarkdown'),
+             gh = c('roxygen2' = 'klutometis/roxygen',
+                    'httpuv' = 'rstudio/httpuv'),
+             bc = c('PhViD',
+                    'Biobase',
+                    'GEOquery'))
+print(list(cran = load_cr(pkgs$cr),
+           github = load_gh(pkgs$gh),
+           bioconductor = load_bc(pkgs$bc)))
