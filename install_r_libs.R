b2f7b6ae7f3da8020a345f1505c82b1e6a54cb3a
dceoy <d.narsil@gmail.com>
2015-12-12 02:32:07 +0900

    Update install_r_libs.R

--- install_r_libs.R
+++ install_r_libs.R
@@ -1,52 +1,57 @@
 #!/usr/bin/env Rscript
 
-options(repos = 'http://cran.rstudio.com/', BioC_mirror = 'https://bioconductor.org')
-r_lib <- .libPaths()[1]
-installed_pkgs <- installed.packages(lib.loc = r_lib)[, 1]
-source('https://bioconductor.org/biocLite.R')
-
 require_v <- function(pkgs) {
   return(sapply(as.vector(pkgs),
                 function(pkg) return(require(pkg, character.only = TRUE))))
 }
 
-load_cran <- function(pkgs) {
-  if(length(pp <- intersect(pkgs, installed_pkgs)) != 0) {
-    if(require_v('devtools')) {
-      with_libpaths(r_lib, update_packages(pp, dependencies = TRUE))
-    } else {
-      update.packages(instPkgs = pp, checkBuilt = TRUE, ask = FALSE, lib.loc = r_lib)
+load_cran <- function(pkgs, r_lib = .libPaths()[1]) {
+  if(! is.null(pkgs)) {
+    options(repos = 'https://cran.rstudio.com/')
+    if(length(pp <- intersect(pkgs, installed.packages(lib.loc = r_lib)[, 1])) > 0) {
+      if(require('devtools')) {
+        with_libpaths(r_lib, update_packages(pp, dependencies = TRUE))
+      } else {
+        update.packages(instPkgs = pp, checkBuilt = TRUE, ask = FALSE, lib.loc = r_lib)
+      }
     }
+    if(length(pd <- setdiff(pkgs, installed.packages(lib.loc = r_lib)[, 1])) > 0) {
+      install.packages(pkgs = pd, lib = r_lib, dependencies = TRUE)
+    }
+    return(require_v(pkgs))
   }
-  if(length(pd <- setdiff(pkgs, installed_pkgs)) != 0) {
-    install.packages(pkgs = pd, lib = r_lib, dependencies = TRUE)
+}
+
+load_dev <- function(pkgs, host = 'GitHub', r_lib = .libPaths()[1]) {
+  if(! is.null(pkgs) && require('devtools')) {
+    try(with_libpaths(r_lib,
+                      switch(host,
+                             'GitHub' = install_github(names(pkgs)),
+                             'Bitbucket' = install_bitbucket(names(pkgs)))))
+    return(require_v(pkgs))
   }
-  return(require_v(pkgs))
 }
 
-load_dev <- function(pkgs, host = 'github') {
-  if(require_v('devtools')) {
-    with_libpaths(r_lib,
-                  switch(host,
-                         'github' = install_github(names(pkgs)),
-                         'bitbucket' = install_bitbucket(names(pkgs))))
+load_bioc <- function(pkgs, r_lib = .libPaths()[1]) {
+  if(! is.null(pkgs)) {
+    options(BioC_mirror = 'https://bioconductor.org')
+    source('https://bioconductor.org/biocLite.R')
+    try(biocLite(pkgs = pkgs, ask = FALSE, lib.loc = r_lib))
+    return(require_v(pkgs))
   }
-  return(require_v(pkgs))
 }
 
-load_bioc <- function(pkgs) {
-  try(biocLite(pkgs = pkgs, ask = FALSE, lib.loc = r_lib))
-  return(require_v(pkgs))
+update_env <- function(ls_pkgs, r_lib = .libPaths()[1]) {
+  load_cran(union('devtools',
+                  union(ls_pkgs$CRAN,
+                        installed.packages(lib.loc = r_lib)[, 1])))
+  load_dev(ls_pkgs$GitHub, host = 'GitHub')
+  load_dev(ls_pkgs$Bitbucket, host = 'Bitbucket')
+  load_bioc(ls_pkgs$Bioconductor)
+  return(print(sapply(ls_pkgs, require_v)))
 }
 
-if(length(installed_pkgs) == 0) {
-  load_cran('devtools')
-} else {
-  load_cran(union('devtools', installed_pkgs))
-  try(biocLite('BiocUpgrade', ask = FALSE, lib.loc = r_lib))
-}
-
-ls_pkgs <- list(cran = c('dplyr',
+update_env(list(CRAN = c('dplyr',
                          'tidyr',
                          'data.table',
                          'yaml',
@@ -76,14 +81,9 @@ ls_pkgs <- list(cran = c('dplyr',
                          'rvest',
                          'rbenchmark',
                          'rmarkdown'),
-                github = c('klutometis/roxygen' = 'roxygen2',
+                GitHub = c('klutometis/roxygen' = 'roxygen2',
                            'rstudio/httpuv' = 'httpuv'),
-                bioc = c('BiocInstaller',
-                         'PhViD',
-                         'Biobase',
-                         'GEOquery'))
-
-print(list(CRAN = c(require_v('devtools'),
-                    load_cran(ls_pkgs$cran)),
-           GitHub = load_dev(ls_pkgs$github),
-           Bioconductor = load_bioc(ls_pkgs$bioc)))
+                Bioconductor = c('BiocInstaller',
+                                 'PhViD',
+                                 'Biobase',
+                                 'GEOquery')))
