3ed531075ec459222450ab0f0625b3000afbc759
dceoy <d.narsil@gmail.com>
2016-07-12 01:07:52 +0900

    Update package_installer.R

--- lib/package_installer.R
+++ lib/package_installer.R
@@ -1,70 +1,67 @@
 #!/usr/bin/env Rscript
 
-require_v <- function(pkgs) {
-  return(sapply(as.vector(pkgs), require, character.only = TRUE))
-}
-
-cran_install <- function(pkgs, cran_repos = getOption('repos'), r_lib = .libPaths()[1]) {
+cran_install <- function(pkgs = NULL, repos = getOption('repos'), r_lib = .libPaths()[1]) {
   if(require('devtools')) {
-    withr::with_libpaths(r_lib,
-                         devtools::update_packages(pkgs = pkgs, repos = cran_repos, dependencies = TRUE))
+    try(withr::with_libpaths(r_lib,
+                             devtools::update_packages(pkgs = pkgs, repos = repos, dependencies = TRUE)))
   } else {
     if(length(pp <- intersect(pkgs, installed.packages(lib.loc = r_lib)[, 1])) > 0) {
-      update.packages(instPkgs = pp, repos = cran_repos, checkBuilt = TRUE, ask = FALSE, lib.loc = r_lib)
+      try(update.packages(instPkgs = pp, repos = repos, checkBuilt = TRUE, ask = FALSE, lib.loc = r_lib))
     }
     if(length(pd <- setdiff(pkgs, installed.packages(lib.loc = r_lib)[, 1])) > 0) {
-      install.packages(pkgs = pd, repos = cran_repos, lib = r_lib, dependencies = TRUE)
+      try(install.packages(pkgs = pd, repos = repos, lib = r_lib, dependencies = TRUE))
     }
   }
 }
 
-devt_install <- function(pkgs, host, cran_repos = getOption('repos'), r_lib = .libPaths()[1]) {
+devtools_install <- function(pkgs, host = 'github', repos = getOption('repos'), r_lib = .libPaths()[1]) {
   if(! is.null(pkgs) && require('devtools')) {
-    withr::with_libpaths(r_lib,
-                         switch(host,
-                                'cran' = devtools::install_cran(pkgs, repos = cran_repos, dependencies = TRUE),
-                                'github' = devtools::install_github(pkgs),
-                                'bitbucket' = devtools::install_bitbucket(pkgs)))
+    try(withr::with_libpaths(r_lib,
+                             switch(host,
+                                    'cran' = devtools::install_cran(pkgs, repos = repos, dependencies = TRUE),
+                                    'github' = devtools::install_github(pkgs),
+                                    'bitbucket' = devtools::install_bitbucket(pkgs))))
   }
 }
 
-drat_install <- function(pkgs, drat_repos, cran_repos = getOption('repos'), r_lib = .libPaths()[1]) {
-  if(! is.null(pkgs) && require('drat')) {
-    options(repos = cran_repos)
-    drat:::addRepo(drat_repos)
-    if(length(pp <- intersect(pkgs, installed.packages(lib.loc = r_lib)[, 1])) > 0) {
-      update.packages(instPkgs = pp, checkBuilt = TRUE, ask = FALSE, lib.loc = r_lib)
-    }
-    if(length(pd <- setdiff(pkgs, installed.packages(lib.loc = r_lib)[, 1])) > 0) {
-      install.packages(pkgs = pd, lib = r_lib, dependencies = TRUE)
-    }
-  }
-}
-
-bioc_install <- function(pkgs, r_lib = .libPaths()[1]) {
+bioc_install <- function(pkgs, mirror = 'https://bioconductor.org', r_lib = .libPaths()[1]) {
   if(! is.null(pkgs)) {
-    options(BioC_mirror = 'https://bioconductor.org')
-    if(! 'biocLite' %in% ls()) source('https://bioconductor.org/biocLite.R')
-    biocLite(pkgs = pkgs, ask = FALSE, lib.loc = r_lib)
+    options(BioC_mirror = mirror)
+    if(! 'biocLite' %in% ls()) source(paste0(mirror, '/biocLite.R'))
+    try(biocLite(pkgs = pkgs, ask = FALSE, lib.loc = r_lib))
   }
+}
+
+test_load <- function(pkgs) {
+  v_status <- suppressMessages(sapply(as.vector(pkgs), require, character.only = TRUE))
+  if(require('devtools')) {
+    message()
+    print(devtools::session_info())
+  }
+  message('\nLoading test -------------------------------------------------------------------')
+  if(length(v_succeeded <- names(v_status)[v_status]) > 0) {
+    message(' Succeeded:')
+    message(paste0(paste0('  ', v_succeeded), collapse = '\n'))
+  }
+  if(length(v_failed <- names(v_status)[! v_status]) > 0) {
+    message(' Failed:')
+    message(paste0(paste0('  ', v_failed), collapse = '\n'))
+  }
+  message()
 }
 
 install_by_list <- function(cfg, r_lib = .libPaths()[1]) {
-  options(repos = cfg$repos$cran)
-  lapply(union(util <- c('devtools', 'drat'), cfg$cran), function(p) try(cran_install(p)))
-  devtools::has_devel()
-  lapply(do.call(c, cfg$github), function(p) try(devt_install(p, host = 'github')))
-  lapply(cfg$drat, function(p) try(drat_install(p, drat_repos = cfg$repos$drat)))
-  lapply(cfg$bioconductor, function(p) try(bioc_install(p)))
-  return(sapply(list(Utility = util,
-                     CRAN = cfg$cran,
-                     GitHub = names(cfg$github),
-                     Drat = cfg$drat,
-                     Bioconductor = cfg$bioc),
-                require_v))
+  options(repos = c(CRAN = cfg$repos$cran))
+  cran_install(util <- c('devtools', 'drat'))
+  sapply(util, require, character.only = TRUE)
+  drat:::addRepo(account = cfg$repos$drat)
+  lapply(c(cfg$cran, cfg$drat), cran_install)
+  lapply(do.call(c, cfg$github), devtools_install)
+  lapply(cfg$bioconductor, bioc_install)
+  test_load(c(cfg$cran, names(cfg$github), cfg$drat, cfg$bioconductor))
 }
 
 if(length(argv <- commandArgs(trailingOnly = TRUE)) > 0) {
-  if(! require('yaml')) cran_install('yaml', cran_repos = 'https://cran.rstudio.com/')
-  print(install_by_list(cfg = yaml::yaml.load_file(input = argv[1])))
+  if(! require('yaml')) cran_install('yaml', repos = 'https://cran.rstudio.com/')
+  install_by_list(cfg = yaml::yaml.load_file(input = argv[1]))
 }
