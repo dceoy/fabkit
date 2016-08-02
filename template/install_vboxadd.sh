d98eb64fb2dbc909f11f05e9ab338a74534c2bf6
dceoy <d.narsil@gmail.com>
2016-08-03 02:26:27 +0900

    Add Vagrantfile

---
+++ template/install_vboxadd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ -f "/etc/lsb-release" ]]; then
+  sudo apt-get -y update
+  sudo apt-get -y install gcc make
+elif [[ -f "/etc/redhat-release" ]]; then
+  if dnf --version > /dev/null 2>&1; then
+    sudo dnf -y update
+    sudo dnf -y install gcc make
+  elif yum --version > /dev/null 2>&1; then
+    sudo yum -y update
+    sudo yum -y install gcc make
+  fi
+fi
+
+vbga_ver="$(curl http://download.virtualbox.org/virtualbox/LATEST.TXT)"
+curl http://download.virtualbox.org/virtualbox/${vbga_ver}/VBoxGuestAdditions_${vbga_ver}.iso -o /tmp/vbga.iso
+sudo mount -t iso9660 /tmp/vbga.iso /mnt/
+sudo /mnt/VBoxLinuxAdditions.run
