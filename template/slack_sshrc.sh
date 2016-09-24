524a4ef50bc9068188ea03790029c16609c1e44e
dceoy <d.narsil@gmail.com>
2016-09-24 17:14:00 +0900

    Add slack_sshrc.sh

---
+++ template/slack_sshrc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# slack notification
+
+set -ue
+
+CHANNEL='#ssh'
+ME="$(whoami)"
+USERNAME="${ME}@$(hostname)"
+TEXT="$(who | grep -e "^${ME} \\+" | tail -1)"
+ICON_EMOJI=':door:'
+WEBHOOK_URL='https://hooks.slack.com/services/xxxxxxxxx/yyyyyyyyy/zzzzzzzzzzzzzzzzzzzzzzzz'
+
+curl -S -s -X POST --data-urlencode \
+  "payload={'channel': '${CHANNEL}', \
+            'username': '${USERNAME}', \
+            'text': '${TEXT}', \
+            'icon_emoji': '${ICON_EMOJI}'}" \
+  ${WEBHOOK_URL} > /dev/null
