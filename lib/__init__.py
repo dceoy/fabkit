d3c0cb66229c17a7ae58208ab2da5116e0ab83c2
dceoy <d.narsil@gmail.com>
2016-11-18 19:11:06 +0900

    Update lib/__init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import shutil
 import os
 
 
 map(lambda d:
     map(lambda f:
-        shutil.copyfile(os.path.join(d, 'default', f), os.path.join(d, f)),
+        os.symlink(os.path.join('default', f), os.path.join(d, f)),
         set(os.listdir(os.path.join(d, 'default'))) - set(os.listdir(d))),
     ('config', 'dotfile'))
